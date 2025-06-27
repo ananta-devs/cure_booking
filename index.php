cfd937705f94c9f5fe07c47c0caa8dafd7a963e3
DIPU-EDITZ <nandidipu717@gmail.com>
2025-06-27 20:40:11 +0530

    playstore

--- index.php
+++ index.php
@@ -163,14 +163,16 @@ $doctors = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     and manage your health records - all from the convenience of your smartphone.</p>
                 <div class="app-buttons">
                     <a href="#" class="app-btn">
-                        <div>
+                        <img class="store" src="http://localhost/cure_booking/assets/play-store.png" />
+                        <div class="btn-text">
                             <span class="app-btn-text-small">Get it on</span>
                             <span class="app-btn-text-large">Google Play</span>
                         </div>
                     </a>
                     <a href="#" class="app-btn">
-                        <div>
-                            <span class="app-btn-text-small">Download on the</span>
+                        <img class="store" src="http://localhost/cure_booking/assets/app-store.png" alt="App Store Logo" />
+                        <div class="btn-text">
+                            <span class="app-btn-text-small">Get it on</span>
                             <span class="app-btn-text-large">App Store</span>
                         </div>
                     </a>
